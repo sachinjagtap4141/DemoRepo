6585eb630be2155145596691c6763efe545531f8
sachinjagtap4141 <sachinjagtap4141@gmail.com>
2021-02-01 13:00:02 +0530

    Changes in login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -3,6 +3,7 @@ package com.qa.pages;
 first time only you have to clone project
 i.e git clone <url>
 
+
 second time onwords no need to clone
 direclty go to directory and pull the code
 like--git pull origin master
